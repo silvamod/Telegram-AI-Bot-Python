17f7d2cd0202476532d3dc4547f9282c95e6d3e6
Mahdius <silvamod@gmail.com>
2023-06-03 17:34:31 +0300

    clean code

--- telegram2.py
+++ telegram2.py
@@ -1,36 +1,38 @@
 import telegram.ext
 import requests
-Token = "INSERT_TOKEN_HERE"
-updater = telegram.ext.Updater("INSERT_TOKEN_HERE",use_context=True)
+import constants
+Token = constants.TOKEN
+updater = telegram.ext.Updater(Token,use_context=True)
 dispatcher = updater.dispatcher
 
 def start(update,context):
-    update.message.reply_text("Hello, what can i do for you?\nType /ask and your request after")
+    update.message.reply_text(constants.START_MSG)
 
 def help(update,context):
-    update.message.reply_text("To start using this bot, type /ask and your request after")
+    update.message.reply_text(constants.HELP_MSG)
 
 def ask(update,context):
     input_txt = update.message.text
     saveLogs(update)
-    if(len(input_txt.split('/ask ')) == 1):
-        update.message.reply_text("Please enter your request after the command /ask with a space.")
+    split_input = input_txt.split(constants.COMMAND_SPLIT)
+    if(len(split_input) == 1):
+        update.message.reply_text(constants.COMMAND_ERROR_MSG)
     else:
-        input_txt_value = input_txt.split('/ask ')[1]
+        input_txt_value = split_input[1]
         context.bot.send_chat_action(chat_id= update.effective_message.chat_id, action=telegram.ChatAction.TYPING)
-        r = requests.get('https://api.mcai.lol/get-result?query='+ input_txt_value)
-        update.message.reply_text(r.text.replace("\\n","\n").replace("\"","") + "\n \n@WhosaAI")
+        r = requests.get(constants.GET_PROMPT_URL+ input_txt_value)
+        update.message.reply_text(r.text.replace("\\n","\n").replace("\"",constants.EMPTY_STR) + "\n \n@WhosaAI")
         
 def saveLogs(update):
-    file = open('logs.txt', 'a', encoding="utf-8")
-    if(update.message.chat.type == "private"):
-        Type = ""
+    file = open(constants.LOG_FILE_PATH, 'a', encoding= constants.UTF_ENCODE)
+    if(update.message.chat.type == constants.PRIVATE_CHAT_TYPE):
+        Type = constants.EMPTY_STR
     else:
         Type = update.message.chat.title
-    chat_id = str(update.message.from_user.id) if(update.message.from_user.id) else ""
-    first_name = str(update.message.from_user.first_name) if(update.message.from_user.first_name) else ""
-    last_name = str(update.message.from_user.last_name) if(update.message.from_user.last_name) else ""
-    username = str(update.message.from_user.username) if(update.message.from_user.username) else ""
+    chat_id = str(update.message.from_user.id) if(update.message.from_user.id) else constants.EMPTY_STR
+    first_name = str(update.message.from_user.first_name) if(update.message.from_user.first_name) else constants.EMPTY_STR
+    last_name = str(update.message.from_user.last_name) if(update.message.from_user.last_name) else constants.EMPTY_STR
+    username = str(update.message.from_user.username) if(update.message.from_user.username) else constants.EMPTY_STR
     file.write("Group: "+ Type +" Chat_id = " +chat_id +" first_name = " + first_name +" last_name = "+last_name + " username = "+ username +" Msg = ")
     file.write(update.message.text)
     file.write("\n")
@@ -38,9 +40,9 @@ def saveLogs(update):
     print(update.message)
 
 
-dispatcher.add_handler(telegram.ext.CommandHandler("start",start))
-dispatcher.add_handler(telegram.ext.CommandHandler("help",help))
-dispatcher.add_handler(telegram.ext.CommandHandler("ask",ask))
+dispatcher.add_handler(telegram.ext.CommandHandler(constants.START,start))
+dispatcher.add_handler(telegram.ext.CommandHandler(constants.HELP,help))
+dispatcher.add_handler(telegram.ext.CommandHandler(constants.ASK,ask))
 
 updater.start_polling()
 updater.idle()
